7d3af07b07c064b96e3507a96a986c79115a04bb
Aashish0510 <0aashishkumar5@gmail.com>
2022-07-07 02:47:05 +0530

    full app

---
+++ lib/screens/onboarding/first_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:buy_mate/constants/color.dart';
+import 'package:buy_mate/functions/next_page.dart';
+import 'package:buy_mate/screens/login/login.dart';
+import 'package:buy_mate/screens/onboarding/second_page.dart';
+import 'package:buy_mate/screens/onboarding/third_page.dart';
+import 'package:buy_mate/widgets/bold_text.dart';
+import 'package:buy_mate/widgets/button_image.dart';
+import 'package:buy_mate/widgets/center_image.dart';
+import 'package:buy_mate/widgets/text_styles.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: const CenterImage(
+                image: 'asset/home.png',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const BoldText(text: 'Get your property that you want with mates'),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SecondPage()));
+              },
+              child: const ButtonImage(),
+            ),
+            TextButton(
+              onPressed: () {
+                nextPage(context: context, widget:  Login());
+              },
+              child: regularText(text: 'Skip', color: kBlackColor),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
